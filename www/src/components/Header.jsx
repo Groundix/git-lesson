cca051fac6230e8aff62ea63d9302e7fc6b41db2
Denis <groundix@mail.ru>
2023-07-22 11:46:10 +0300

    added first project version www folder

---
+++ www/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Header = () =>{
+    return <header className="header">
+         <header className='header'> 
+         <img src='https://www.pngarts.com/files/3/Logo-PNG-Transparent-Image.png' />
+        </header>
+    </header>
+}
+
+export  default Header;
---
+++ www/src/components/Profile.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className='content'>
+        <div className="profile-banner">
+          <div className="media">
+            <h3>Denis Stoico</h3>
+          </div>
+          <div className="media-body">
+
+          </div>
+       </div>
+        <div className="avatar">
+          
+          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwLqUvs7hnvYYbrSL3QndEhPXhsN7gn19jZKjCinIUt4dssMbi7ChuLbUrncJckjtoxgs&usqp=CAU'/>
+          
+        </div>
+        <div>
+          My posts
+          <div>
+            new posts
+          </div>
+        </div>
+        <div>
+          Post 1
+        </div>
+        <div>
+          Post 2
+          </div>
+      </div>
+}
+
+export default Profile;
